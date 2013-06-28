7e291b037873c3c17557fe6179c071022c6476db
Michael Witwicki <michael@fromtheoutfit.com>
2013-06-28 09:31:37 -0400

    Improved comments

--- system/expressionengine/third_party/playa_links/views/mcp/display_settings.php
+++ system/expressionengine/third_party/playa_links/views/mcp/display_settings.php
@@ -6,11 +6,11 @@
     $this->table->set_template($cp_table_template);
     $this->table->set_heading(array(lang('playa_links_setting'),lang('playa_links_preference')));
 
-//$form .= form_label('Target', 'target') . NBS . );
-
+    // Add rows for each of the link types
     $this->table->add_row($child_language);
     $this->table->add_row($parent_language);
 
+    // Generate the table
     $data .= $this->table->generate();
 
     echo $data;
